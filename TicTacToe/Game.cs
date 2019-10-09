65272177b744696ee43aff319817e0f391c9940b
Mike Gorzelsky <m.d.gorzelsky@gmail.com>
2019-10-08 19:42:29 -0700

    Added Instructions and text displaying the winner.

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -13,74 +13,86 @@ namespace TicTacToe
         Player playerO = new Player(CurrentState.O);
         public void PlayGame()
         {
+            string gameResult = "continue";
             playArea.DrawBoard();
 
             //The loop that handles the game play.
             while (true)
             {
                 playArea.TakeTurn(playerX.GetPlayer());
-                if (WinCheck())
+                gameResult = WinCheck();
+                if (gameResult == "X" || gameResult == "O" || gameResult == "DRAW")
                     break;
 
                 playArea.TakeTurn(playerO.GetPlayer());
-                if (WinCheck())
+                gameResult = WinCheck();
+                if (gameResult == "X" || gameResult == "O" || gameResult == "DRAW")
                     break;
             }
+            
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine("\r" + new string(' ', Console.WindowWidth) + "\r");
+            Console.SetCursorPosition(10, 10);
+
+            if (gameResult == "X" || gameResult == "O")
+                Console.WriteLine("Congratulations player " + gameResult + "! You've won the game!");
+            else
+                Console.WriteLine("Well it seems it was a " + gameResult + "! Better Luck Next time!");
         }
-        private bool WinCheck()
+        private string WinCheck()
         {
             CurrentState[,] currentGameBoard = playArea.GetState();
             //Horizontal X wins
             if (currentGameBoard[0, 0] == CurrentState.X && currentGameBoard[0, 1] == CurrentState.X && currentGameBoard[0, 2] == CurrentState.X)
-                return true;
+                return "X";
             if (currentGameBoard[1, 0] == CurrentState.X && currentGameBoard[1, 1] == CurrentState.X && currentGameBoard[1, 2] == CurrentState.X)
-                return true;
+                return "X";
             if (currentGameBoard[2, 0] == CurrentState.X && currentGameBoard[2, 1] == CurrentState.X && currentGameBoard[2, 2] == CurrentState.X)
-                return true;
+                return "X";
 
             //Vertical X wins
             if (currentGameBoard[0, 0] == CurrentState.X && currentGameBoard[1, 0] == CurrentState.X && currentGameBoard[2, 0] == CurrentState.X)
-                return true;
+                return "X";
             if (currentGameBoard[0, 1] == CurrentState.X && currentGameBoard[1, 1] == CurrentState.X && currentGameBoard[2, 1] == CurrentState.X)
-                return true;
+                return "X";
             if (currentGameBoard[0, 2] == CurrentState.X && currentGameBoard[1, 2] == CurrentState.X && currentGameBoard[2, 2] == CurrentState.X)
-                return true;
+                return "X";
 
             //Diagonal X Wins
             if (currentGameBoard[0, 0] == CurrentState.X && currentGameBoard[1, 1] == CurrentState.X && currentGameBoard[2, 2] == CurrentState.X)
-                return true;
+                return "X";
             if (currentGameBoard[0, 2] == CurrentState.X && currentGameBoard[1, 1] == CurrentState.X && currentGameBoard[2, 0] == CurrentState.X)
-                return true;
+                return "X";
 
             //Horizontal O wins
             if (currentGameBoard[0, 0] == CurrentState.O && currentGameBoard[0, 1] == CurrentState.O && currentGameBoard[0, 2] == CurrentState.O)
-                return true;
+                return "O";
             if (currentGameBoard[1, 0] == CurrentState.O && currentGameBoard[1, 1] == CurrentState.O && currentGameBoard[1, 2] == CurrentState.O)
-                return true;
+                return "O";
             if (currentGameBoard[2, 0] == CurrentState.O && currentGameBoard[2, 1] == CurrentState.O && currentGameBoard[2, 2] == CurrentState.O)
-                return true;
+                return "O";
 
             //Vertical O wins
             if (currentGameBoard[0, 0] == CurrentState.O && currentGameBoard[1, 0] == CurrentState.O && currentGameBoard[2, 0] == CurrentState.O)
-                return true;
+                return "O";
             if (currentGameBoard[0, 1] == CurrentState.O && currentGameBoard[1, 1] == CurrentState.O && currentGameBoard[2, 1] == CurrentState.O)
-                return true;
+                return "O";
             if (currentGameBoard[0, 2] == CurrentState.O && currentGameBoard[1, 2] == CurrentState.O && currentGameBoard[2, 2] == CurrentState.O)
-                return true;
+                return "O";
 
             //Diagonal O Wins
             if (currentGameBoard[0, 0] == CurrentState.O && currentGameBoard[1, 1] == CurrentState.O && currentGameBoard[2, 2] == CurrentState.O)
-                return true;
+                return "O";
             if (currentGameBoard[0, 2] == CurrentState.O && currentGameBoard[1, 1] == CurrentState.O && currentGameBoard[2, 0] == CurrentState.O)
-                return true;
+                return "O";
 
             //Draw condition
             if (currentGameBoard[0, 0] != CurrentState.Empty && currentGameBoard[0, 1] != CurrentState.Empty && currentGameBoard[0, 2] != CurrentState.Empty &&
                 currentGameBoard[1, 0] != CurrentState.Empty && currentGameBoard[1, 1] != CurrentState.Empty && currentGameBoard[1, 2] != CurrentState.Empty &&
                 currentGameBoard[2, 0] != CurrentState.Empty && currentGameBoard[2, 1] != CurrentState.Empty && currentGameBoard[2, 2] != CurrentState.Empty)
-                return true;
+                return "DRAW";
 
-            else { return false; }
+            else { return "contine"; }
         }
     }
 }
